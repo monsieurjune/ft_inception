33beb69a4dc61d7bf1a3a6ac37a9f451fff7dc9a
monsieurjune <june.monsieurjune@gmail.com>
2025-04-30 02:16:46 +0700

    (fix): Fix to use echo to append sql, instead of printf
    Fix:
    - Convert to use echo, instead of printf, due to non translate special
      character (e.g. \n)
    - Remove every parameter in mariadb config file, only left listening ip
      and port

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -43,7 +43,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     sleep 5
 
     # Append SQL to MariaDB
-    printf "%s" "${SQL_COMMANDS}" | mariadb -u root
+    echo "${SQL_COMMANDS}" | mariadb -u root
 
     # Stop Temporary DB
     mariadb-admin shutdown
