327481a988a4ff1a8e577765be8a005bf5385550
monsieurjune <june.monsieurjune@gmail.com>
2025-04-30 02:33:02 +0700

    (feat): Create simple wordpress image with tools

---
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+
+# Set to exit when cmd return non-zero
+set -e
+
+# Get env from credential file
+set -a
+. /run/secrets/credentials
+set +a
+
+# Set Variable
+WORDPRESS_URL="https://${DOMAIN_NAME}:8443"
+
+# Create php directories
+if [ ! -d /run/php ]; then
+    mkdir -p /run/php
+fi
+
+# Wait for db to work
+sleep 10
+
+# Change directory
+cd /var/www/html
+
+# Create wp-config.php
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create \
+        --dbhost="${WORDPRESS_DB_HOST}" \
+        --dbname="${WORDPRESS_DB_NAME}" \
+        --dbuser="${WORDPRESS_DB_USER}" \
+        --dbpass="$(cat ${WORDPRESS_DB_PASSWORD_FILE})" \
+        --path=/var/www/html \
+        --allow-root
+fi
+
+# Install wordpress
+if ! wp core is-installed --allow-root --path="/var/www/html"; then
+    wp core install \
+        --url="${WORDPRESS_URL}" \
+        --title="${WORDPRESS_TITLE}" \
+        --admin_user="${WORDPRESS_ADMIN_USER}" \
+        --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+        --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+        --skip-email \
+        --path=/var/www/html \
+        --allow-root
+fi
+
+# Start php-fpm & lighttpd
+exec php-fpm82 --nodaemonize
---
+++ srcs/requirements/wordpress/tools/download-wordpress.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# Exit when error
+set -e
+
+# Variable
+DOWNLOAD_DIR=./srcs/requirements/wordpress/tools/
+WORDPRESS_LINK=https://wordpress.org/wordpress-6.8.tar.gz
+WORDPRESS_CLI_LINK=https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+WORDPRESS_FILE=wordpress.tar.gz
+WORDPRESS_CLI_FILE=wp-cli.phar
+
+# Exit when error
+set -e
+
+# Download Wordpress 6.8
+if [ ! -f "${DOWNLOAD_DIR}/${WORDPRESS_FILE}" ]; then
+    wget ${WORDPRESS_LINK} -O ${DOWNLOAD_DIR}/${WORDPRESS_FILE}
+fi
+
+# Download wp-cli
+if [ ! -f "${DOWNLOAD_DIR}/${WORDPRESS_CLI_FILE}" ]; then
+    curl -o ${DOWNLOAD_DIR}/${WORDPRESS_CLI_FILE} ${WORDPRESS_CLI_LINK}
+fi
+
+# Extract file
+if [ ! -d "${DOWNLOAD_DIR}/wordpress" ]; then
+    tar -xzvf ${DOWNLOAD_DIR}/${WORDPRESS_FILE} -C ${DOWNLOAD_DIR}
+fi
